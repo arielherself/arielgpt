10ff1b3eab9d2a13ee683659adbd9e3f3f9fed0f
ariel <ariel@arielherself.xyz>
2023-02-17 14:49:10 +0800

    18

--- main.py
+++ main.py
@@ -57,6 +57,7 @@ async def reply(message: telebot.types.Message) -> int:
                 l = message.text.split(' ', 1)
                 if len(l) == 1:
                     cmd = l[0]
+                    cmd = cmd[:cmd.find('@')]
                     arg = ''
                 else:
                     cmd, arg = l
